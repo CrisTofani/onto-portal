3237abd50ffb979b7f8a6b1d2f16272467562687
Simonetta Andrea <A.Simonetta@almaviva.it>
2018-04-23 14:57:36 +0200

    Added sources dynamism to `./config/serviceURI.js`

--- src/config/serviceURI.js
+++ src/config/serviceURI.js
@@ -1,8 +1,9 @@
-const mainURI = 'http://dafdocker.westeurope.cloudapp.azure.com'
+const mainURI = window.location.hostname === 'localhost' ?
+  'http://localhost' : 'http://dafdocker.westeurope.cloudapp.azure.com'
 
 export default {
-  api_kataLOD: mainURI.concat(':7777/kb/api/v1'),
-  // api_LodView: mainURI.concat(':8080/lodview'),
-  api_LODE: mainURI.concat(':9090/lode/extract?url='),
-  api_WebVOWL: mainURI.concat(':8080/webvowl/#iri=')
+  api_kataLOD: `${mainURI}:7777/kb/api/v1`,
+  // api_LodView: `${mainURI}:8080/lodview`,
+  api_LODE: `${mainURI}:9090/lode/extract?url=`,
+  api_WebVOWL: `${mainURI}:8080/webvowl/#iri=`,
 }
