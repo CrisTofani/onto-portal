2219550dd04f30c2f5ae2e3c5f37bec00dcff3c7
Simonetta Andrea <a.simonetta@almaviva.it>
2018-05-04 00:34:10 +0200

    template string literals refactorings...

--- src/components/OntologiesList/OntologiesList.js
+++ src/components/OntologiesList/OntologiesList.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import {
-  Container, Row, Col,
-  Card, CardBody, CardTitle, CardSubtitle, CardText, CardFooter,
-  Alert, Badge
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  CardFooter,
+  Alert,
+  Badge
 } from 'reactstrap'
 
 import LinkWrap from '../LinkWrap'
@@ -11,68 +19,57 @@ import Error from '../Error'
 const ontologiesError = 'Errore durante il caricamento delle ontologie'
 
 const mapOntologies = ontologies =>
-  ontologies.map(ont =>
-    <LinkWrap
-      key={ont.id}
-      to={"/ontologies/".concat(ont.id)}
-      className="text-dark"
-    >
+  ontologies.map(ont => (
+    <LinkWrap key={ont.id} to={`/ontologies/${ont.id}`} className="text-dark">
       <Card className="border-0">
         <CardBody>
-
-          <CardTitle className="text-primary">
-            {ont.titles[0].value}
-          </CardTitle>
-          <CardSubtitle>
-            {ont.id}
-          </CardSubtitle>
-          <CardText>
-            {ont.descriptions[0].value}
-          </CardText>
+          <CardTitle className="text-primary">{ont.titles[0].value}</CardTitle>
+          <CardSubtitle>{ont.id}</CardSubtitle>
+          <CardText>{ont.descriptions[0].value}</CardText>
 
           <Alert color="secondary" className="p-0 mb-0 border-0">
-            <Badge color="primary" className="px-3 py-2">Ontologia</Badge>
+            <Badge color="primary" className="px-3 py-2">
+              Ontologia
+            </Badge>
             <strong className="px-2">TAG: </strong>
             {ont.tags.map(tag => tag.value).join(' - ')}
           </Alert>
-
         </CardBody>
       </Card>
     </LinkWrap>
-)
+  ))
 
-const createOntologies = (ontologies) => {
+const createOntologies = ontologies => {
   return (
-  <Row>
-    <Col sm={2}></Col>
-    <Col sm={8}>
-      {mapOntologies(ontologies)}
-    </Col>
-    <Col sm={2}>
-      <div className="callout callout-primary">
-        <small className="text-muted">Risultato della ricerca</small>
-        <br />
+    <Row>
+      <Col sm={2} />
+      <Col sm={8}>{mapOntologies(ontologies)}</Col>
+      <Col sm={2}>
+        <div className="callout callout-primary">
+          <small className="text-muted">Risultato della ricerca</small>
+          <br />
 
-        <strong className="h4">
-          {ontologies.length}
-        </strong>
-        <strong>
-          {ontologies.length > 1 ? "ontologie" : "ontologia"}
-        </strong>
-
-      </div>
-    </Col>
-  </Row>
+          <strong className="h4">{ontologies.length}</strong>
+          <strong>{ontologies.length > 1 ? 'ontologie' : 'ontologia'}</strong>
+        </div>
+      </Col>
+    </Row>
   )
 }
 
 export default class OntologiesList extends React.Component {
-  constructor(props) { super(props) }
-  componentWillMount() { this.props.fetchOntList() }
+  constructor(props) {
+    super(props)
+  }
+  componentWillMount() {
+    this.props.fetchOntList()
+  }
 
   render() {
-    return this.props.hasFetched
-      ? (<Container fluid>{ createOntologies(this.props.data) }</Container>)
-      : this.props.error ? <Error msg={ontologiesError} /> : null
+    return this.props.hasFetched ? (
+      <Container fluid>{createOntologies(this.props.data)}</Container>
+    ) : this.props.error ? (
+      <Error msg={ontologiesError} />
+    ) : null
   }
 }
--- src/util/mapLangProps.js
+++ src/util/mapLangProps.js
@@ -2,36 +2,44 @@ const isEmptyArray = arr => arr.length === 0
 
 const hasArrayValue = value => Array.isArray(value)
 
-const hasLangValues = (values) => {
-  const langValues =
-    values.filter(item => Object.prototype.hasOwnProperty.call(item, 'lang'))
+const hasLangValues = values => {
+  const langValues = values.filter(item =>
+    Object.prototype.hasOwnProperty.call(item, 'lang')
+  )
 
   return !isEmptyArray(langValues)
 }
 
-const defaultLangValue = (lang) => {
+const defaultLangValue = lang => {
   switch (lang) {
-  case 'it': return [{ lang: lang, value: 'Non disponibile in italiano' }]
-  case 'en': return [{ lang: lang, value: 'Not available for english' }]
-  default: return [{ lang: lang, value: 'Not available for '.concat(lang) }]
+    case 'it':
+      return [{ lang: lang, value: 'Non disponibile in italiano' }]
+    case 'en':
+      return [{ lang: lang, value: 'Not available for english' }]
+    default:
+      return [{ lang: lang, value: `Not available for ${lang}` }]
   }
 }
 
 const filterLangValues = ([key, values], lang) => {
   const langValue = values.filter(item => item.lang === lang)
-  return isEmptyArray(langValue) ?
-    [key, defaultLangValue(lang)] : [key, langValue]
+  return isEmptyArray(langValue)
+    ? [key, defaultLangValue(lang)]
+    : [key, langValue]
 }
 
 export const mapLangProps = (data, lang) => {
   const filterLangData = Object.entries(data)
-    .filter(pair =>
-      hasArrayValue(pair[1]) &&
-      (hasLangValues(pair[1]) || isEmptyArray(pair[1]))
+    .filter(
+      pair =>
+        hasArrayValue(pair[1]) &&
+        (hasLangValues(pair[1]) || isEmptyArray(pair[1]))
     )
     .map(pair => filterLangValues(pair, lang))
-    .reduce((accumulator, pair) =>
-      Object.assign(accumulator, { [pair[0]]: pair[1] }), {})
+    .reduce(
+      (accumulator, pair) => Object.assign(accumulator, { [pair[0]]: pair[1] }),
+      {}
+    )
 
   return Object.assign(data, filterLangData)
 }
