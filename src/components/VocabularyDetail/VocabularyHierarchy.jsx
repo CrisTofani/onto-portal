3cbc5e2cb741162617916a889703c7a8b0296cd9
Simonetta Andrea <a.simonetta@almaviva.it>
2018-05-07 12:48:14 +0200

    Moved 'isEmptyArray' to 'isEmpty' from 'commonUtils'

--- src/components/VocabularyDetail/VocabularyHierarchy.jsx
+++ src/components/VocabularyDetail/VocabularyHierarchy.jsx
@@ -2,19 +2,22 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
 import Collapsible from 'react-collapsible'
 
-const isEmptyArray = array => array.length === 0
-
-const genTreeview = hierarchy =>
-  <ListGroup>{
-    hierarchy.map((node, index) => {
-      const listItem = props =>
+import { isEmpty } from '../../util/commonUtils'
+const genTreeview = hierarchy => (
+  <ListGroup>
+    {hierarchy.map((node, index) => {
+      const listItem = props => (
         <ListGroupItem {...props}>{node.label}</ListGroupItem>
-      return isEmptyArray(node.children)
-        ? listItem({ key: index })
-        : <Collapsible key={index} trigger={listItem()}>
+      )
+      return isEmpty(node.children) ? (
+        listItem({ key: index })
+      ) : (
+        <Collapsible key={index} trigger={listItem()}>
           {genTreeview(node.children)}
         </Collapsible>
-    })
-  }</ListGroup>
+      )
+    })}
+  </ListGroup>
+)
 
-export default props => props.hierarchy ? genTreeview(props.hierarchy) : null
+export default props => (props.hierarchy ? genTreeview(props.hierarchy) : null)
